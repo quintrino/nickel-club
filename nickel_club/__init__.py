65cbe1e5dab82d83b1531bde1225717b81a0fced
James Sully <sullyj3@gmail.com>
2022-03-21 13:05:49 +1100

    Separate dev and prod configuration

--- nickel_club/__init__.py
+++ nickel_club/__init__.py
@@ -3,22 +3,26 @@ __version__ = '0.1.0'
 import os
 
 from flask import Flask
+from dotenv import load_dotenv
+
+# Load environment variables from .env file, if it exists.
+# Does not override existing env variables. In this way, we can store dev 
+# configuration in a .env file that is not committed, while configuring on heroku using regular env variables
+load_dotenv()
 
 
-# def create_app(test_config=None):
-def create_app():
+def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
 
-    # TODO: this is local dev only
-    app.config['SECRET_KEY'] = "dev"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://james@localhost/testDatabase'
-    # if test_config is None:
-    #     # load the instance config, if it exists, when not testing
-    #     app.config.from_pyfile("config.py", silent=True)
-    # else:
-    #     # load the test config if passed in
-    #     app.config.update(test_config)
+    if test_config is None:
+        app.config.from_mapping({
+            "SECRET_KEY": os.getenv("SECRET_KEY"),
+            "SQLALCHEMY_DATABASE_URI": os.getenv("DATABASE_URL")
+        })
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
 
     from nickel_club import model
     model.init_app(app)
