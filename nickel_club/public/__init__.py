c6a019ec0cd65e248e8eee0c859309daac9bf04f
James Sully <sullyj3@gmail.com>
2022-03-23 16:38:32 +1100

    Create individual member page

--- nickel_club/public/__init__.py
+++ nickel_club/public/__init__.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
-from flask import render_template
+from flask import Blueprint, render_template, abort
+
+from nickel_club.model import ClubMember
 
 bp = Blueprint("public", __name__, template_folder='templates')
 
@@ -7,3 +8,10 @@ bp = Blueprint("public", __name__, template_folder='templates')
 @bp.route("/about", methods=("GET",))
 def about():
     return render_template("public/about.html")
+
+@bp.route("/member/<int:member_id>", methods=["GET"])
+def member(member_id):
+    member = ClubMember.query.filter_by(id=member_id).first()
+    if member is None:
+        abort(400)
+    return render_template("public/member.html", member=member)
