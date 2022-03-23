2bfa9ec4c9fee15a49d5ec05bd90000f0fdb60b3
James Sully <sullyj3@gmail.com>
2022-03-23 17:15:35 +1100

    Add leaderboard

--- nickel_club/public/__init__.py
+++ nickel_club/public/__init__.py
@@ -13,3 +13,8 @@ def about():
 def member(member_id):
     member = ClubMember.query.get_or_404(member_id)
     return render_template("public/member.html", member=member)
+
+@bp.route("/leaderboard", methods=["GET"])
+def leaderboard():
+    members = ClubMember.query.order_by(ClubMember.nickels.desc()).all()
+    return render_template("public/leaderboard.html", members=members)
